c6c02ddfe0d414acf14a43498bd1b9f1d8c3c928
Alexis Mermet <alexis.mermet@gmail.com>
2022-02-06 23:06:55 +0100

    fix

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -36,4 +36,4 @@ if (!isConnect()) {
 </form>
 
 <!-- Inclusion du fichier javascript du plugin (dossier, nom_du_fichier, extension_du_fichier, id_du_plugin) -->
-<?php include_file('plugin_info', 'configuration', 'js', 'veralink');?>
+<script type="text/javascript" src="/plugins/veralink/plugin_info/configuration.js"> </script>
