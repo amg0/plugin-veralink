218bea370fd8ad14ada2258900bb025f532ad73d
amg0 <alexis.mermet@gmail.com>
2022-02-20 20:36:01 +0100

    test (#34)

--- core/class/veralink.class.php
+++ core/class/veralink.class.php
@@ -17,7 +17,7 @@
  */
 
 /* * ***************************Includes********************************* */
-
+// test
 require_once __DIR__  . '/../../../../core/php/core.inc.php';
 
 const VERALINK = 'veralink';     // plugin logical name
